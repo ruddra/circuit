d476731f0e846df248e8cf58777131b6bdaab005
Arnab Kumar Shil <ruddra90@gmail.com>
2022-01-06 16:24:10 +0100

    add factory

---
+++ app/src/main/java/circuit/Circuit.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package circuit;
+
+/**
+ *
+ * @author arnabkumarshil
+ */
+/*interface*/
+abstract class Circuit {
+
+    public abstract boolean getResult();
+    
+}
+
+abstract class BinaryGate extends Circuit {
+
+    protected Circuit left;
+    protected Circuit right;
+
+    public void setOperand(Circuit left, Circuit right) {
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+abstract class Unary extends Circuit {
+
+    protected Circuit left;
+    protected boolean value;
+
+    public void setOperand(Circuit left) {
+        this.left = left;
+    } 
+
+}
+
+class Not extends Unary {
+
+    
+    public boolean getResult() {
+        return !this.left.getResult();
+    }
+
+}
+
+class And extends BinaryGate {
+
+    public boolean getResult() {
+        return this.left.getResult() && this.right.getResult();
+    }
+
+}
+
+class Or extends BinaryGate {
+
+    public boolean getResult() {
+        return this.left.getResult() || this.right.getResult();
+    }
+
+}
+
+class Constant extends Circuit {
+	private boolean operand;
+
+    public boolean getResult() {
+        return operand;
+    }
+
+    public Constant(boolean operand) {
+        this.operand = operand;
+
+    }
+}
---
+++ app/src/main/java/circuit/CircuitFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package circuit;
+
+/**
+ *
+ * @author arnabkumarshil
+ */
+public class CircuitFactory {
+
+    public Circuit circuit;
+
+    public And getAnd(Circuit left, Circuit right) {
+        And and = new And();
+        and.setOperand(left, right);
+        return and;
+    }
+
+    public Or getOr(Circuit left, Circuit right) {
+        Or or = new Or();
+        or.setOperand(left, right);
+        return or;
+    }
+
+    public Not getNot(Circuit left) {
+        Not not = new Not();
+        not.setOperand(left);
+        return not;
+    }
+
+    public Constant getTrueConstant() {
+        return new Constant(true);
+    }
+
+    public Constant getFalseConstant() {
+        return new Constant(false);
+    }
+
+}
